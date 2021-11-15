818119d3fa423c2abef00ebd0f8ffb3a1d1fa0c7
Cicero Gasparini Brunelli <brunelli.cg@gmail.com>
2021-11-15 18:34:53 -0300

    inital

--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
@@ -6,7 +6,13 @@ class TurnUserAdminController {
   constructor(private turnUserAdminUseCase: TurnUserAdminUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    try {
+      const { user_id } = request.body;
+      this.turnUserAdminUseCase.execute(user_id);
+      return response.status(200).send();
+    } catch (err) {
+      return response.status(400).json({ error: err.message });
+    }
   }
 }
 
